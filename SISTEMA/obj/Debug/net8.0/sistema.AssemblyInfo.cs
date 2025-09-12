ba508ef97ac9a309b0bfe8a9482276929edd72ab
gleison <gleison@localhost.localdomain>
2025-09-11 21:07:40 -0300

    UPDATE

--- SISTEMA/obj/Debug/net8.0/sistema.AssemblyInfo.cs
+++ SISTEMA/obj/Debug/net8.0/sistema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5816a971133d9196dfcd2ebf9986d79bbb503b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648fdbd0ff10d7a0ecca36db39d3061f1452c4e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
